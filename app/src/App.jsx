3cbcadc3e5a2cc5d565dce5ff3bbe0294b32f8e5
letdayy <lcdayane49@gmail.com>
2023-01-18 19:09:31 -0300

    base do projeto

---
+++ app/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Inicio from './pages/Inicio';
+
+function App() {
+  return (
+    <>
+      <Inicio />
+    </>
+  );
+}
+
+export default App;
---
+++ app/src/pages/Inicio/index.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import './style.css';
+
+function Home() {
+    return (
+        <nav>
+            <img
+                src=""
+                width="100"
+            />
+            <div className="search">
+                <img
+                    src=""
+                />
+                <input text="" />
+                <img
+                    src=""
+                />
+            </div>
+            <ul>
+                <li>
+                    <a className="color" href="/">Home&nbsp;&nbsp;</a>|
+                </li>
+                <li>
+                    <a className="color" href="/">Criar Conta &nbsp;&nbsp;</a>|
+                </li>
+                <li>
+                    <a className="color" href="/">Entrar</a>
+                </li>
+                <li>
+                    <a href="/">
+                        <img
+                            src=""
+                            width="21px"
+                        />
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Home;
