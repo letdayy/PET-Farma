b2b29215bc1ec115585dd0ac3f0b7fe9b7b2de93
letdayy <108416308+letdayy@users.noreply.github.com>
2023-03-03 20:10:50 -0300

    Add files via upload

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import Logo from '../../images/logo.png';
+import SlidePrincipal from '../../images/petfarma.png';
 import Login from '../../images/login.png';
 import './style.css';
 
@@ -7,7 +8,7 @@ export default function Home() {
   return (
     <>
       <nav>
-        <img src={Logo} className="logo"/>
+        <img src={Logo} alt="logo do site" className="logo"/>
 
         <input type="text" />
 
@@ -16,10 +17,40 @@ export default function Home() {
             <Link to="">Inicio </Link>
             <Link to="">Contato </Link>
             <Link to="">Sobre </Link>
-            <Link to=""><img src={Login} /> </Link>
+            <Link to=""><img src={Login} alt="imagem de login"/> </Link>
           </li>
         </ul>
       </nav>
+
+      <body>
+        <section className="imgSection">
+        <img src={SlidePrincipal} alt="Slide principal"/>
+        </section>
+
+        <section className="medicationSection">
+          <h1>
+            Medicamentos da Farmácia Universitária
+          </h1>
+          <div className="medicationContent">
+          <p>
+            Possuem maior risco de interação
+          </p>
+          <ul>
+            <li><Link to="">Hidroxicloroquina</Link></li>
+            <li><Link to="">Atorvastatina</Link></li>
+            <li><Link to="">Sulfato de Neomicina</Link></li>
+          </ul>
+
+          </div>
+            
+        </section>
+
+        <footer>
+          <p>
+          Feito por Letícia Dayane
+          </p>
+        </footer>
+      </body>
     </>
   );
 }
